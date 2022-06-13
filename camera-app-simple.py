75c3c3d6ed5d5c25867e244c2a0fa190c5db5efb
Luna Nane <luna@m-box.de>
2022-06-13 16:02:41 +0200

    added google colabs

--- camera-app-simple.py
+++ camera-app-simple.py
@@ -8,29 +8,36 @@ model = models.load_model('model-00001.h5')
 video = cv2.VideoCapture(0)
 
 while True:
-	_, frame = video.read()
+    _, frame = video.read()
 
-	#Convert the captured frame into RGB
-	im = Image.fromarray(frame, 'RGB')
+    #Convert the captured frame into RGB
+    im = Image.fromarray(frame, 'RGB')
 
-	#Resizing into 128x128 because we trained the model with this image size.
-	im = im.resize((100,100))
-	img_array = np.array(im)
+    #Resizing into 128x128 because we trained the model with this image size.
+    im = im.resize((100,100))
+    img_array = np.array(im)
 
-	#Our keras model used a 4D tensor, (images x height x width x channel)
-	#So changing dimension 128x128x3 into 1x128x128x3 
-	img_array = np.expand_dims(img_array, axis=0)
-	img_array = np.expand_dims(img_array, axis=0)
-	#Calling the predict method on model to predict 'me' on the image
-	prediction = int(model.predict(img_array)[0][0])
+    #Our keras model used a 4D tensor, (images x height x width x channel)
+    #So changing dimension 128x128x3 into 1x128x128x3 
 
-	#if prediction is 0, which means I am missing on the image, then show the frame in gray color.
-	if prediction == 0:
-		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = np.expand_dims(img_array, axis=0)
+    
+    #print(img_array)
 
-	cv2.imshow("Capturing", frame)
-	key=cv2.waitKey(1)
-	if key == ord('q'):
-		break
+    #Calling the predict method on model to predict 'me' on the image
+    prediction = int(model.predict(img_array)[0][0])
+
+    #if prediction is 0, which means I am missing on the image, then show the frame in gray color.
+    if prediction == 0:
+    
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow("Capturing", frame)
+    key=cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+        
 video.release()
 cv2.destroyAllWindows()
